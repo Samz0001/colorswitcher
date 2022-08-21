5d8e56e1a9bda857cbc5cbe774b69dedf5a282e2
sishere23 <shyamduaoppo@gmail.com>
2022-08-21 14:43:09 +0530

    Color switcher

---
+++ app.js
@@ -0,0 +1,20 @@
+const body = document.body;
+const switchBtn = document.querySelector(".switch");
+
+const switchPara = document.querySelector(".color");
+
+switchBtn.addEventListener("click", function () {
+  let color1 = getRandomNum(); // red
+  let color2 = getRandomNum(); // green
+  let color3 = getRandomNum(); // blue
+
+  const colorString = `rgb(${color1},${color2},${color3})`;
+  console.log(colorString);
+
+  body.style.backgroundColor = colorString;
+  switchPara.innerText = colorString;
+});
+
+function getRandomNum() {
+  return Math.floor(Math.random() * 256);
+}
